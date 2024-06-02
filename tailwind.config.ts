adea6fbbdd35e07b0fd97ec08da0cc0584e08b8e
portal martin <ptl_martin@yahoo.co.jp>
2024-06-02 17:58:38 +0900

    chore: Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,19 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
+
+// remember that tailwind is mobile first, so if you want to change the default breakpoint you can do it in the tailwind.config.js file
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
     container: {
+      // this is shadcn-ui configurtion ***no remove***
       center: true,
       padding: "2rem",
       screens: {
@@ -18,7 +21,11 @@ const config = {
       },
     },
     extend: {
+      colors: {
+        // here custum colors
+      },
       keyframes: {
+        // this is shadcn-ui configurtion ***no remove***
         "accordion-down": {
           from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
@@ -29,12 +36,13 @@ const config = {
         },
       },
       animation: {
+        // this is shadcn-ui configurtion ***no remove***
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
